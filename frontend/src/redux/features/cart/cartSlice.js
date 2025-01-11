b0885aedad6a25dad52f96789cbba1653a9b690d
MANOJ-80 <manoj.xakep@gmail.com>
2025-01-11 12:42:42 +0530

    Redux Init-3

--- frontend/src/redux/features/cart/cartSlice.js
+++ frontend/src/redux/features/cart/cartSlice.js
@@ -1,4 +1,5 @@
 import { createSlice } from "@reduxjs/toolkit";
+import Swal from "sweetalert2";
 //import Swal  from "sweetalert2";
 
 const initialState = {
@@ -13,9 +14,26 @@ const cartSlice = createSlice({
             const existingItem = state.cartItems.find(item => item._id === action.payload._id);
             if(!existingItem){
                 state.cartItems.push(action.payload)
-                alert('Item Added successfully')
+               
+ Swal.fire({
+  position: "top-end",
+  icon: "success",
+  title: "Product Added to the Cart",
+  showConfirmButton: false,
+  timer: 1500
+});
             } else{
-                alert('Item already in cart')
+             
+               
+                Swal.fire({
+                  title: "Already Added to the Cart",
+                  text: "You won't be able to revert this!",
+                  icon: "warning",
+                  showCancelButton: true,
+                  confirmButtonColor: "#3085d6",
+                  cancelButtonColor: "#d33",
+                  confirmButtonText: "Ok"
+                })
             }
         }
     }
