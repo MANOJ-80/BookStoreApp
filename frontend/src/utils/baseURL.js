91c5bcf7ee3d65ecae238137e1afaab245cbfdfe
MANOJ-80 <manoj.xakep@gmail.com>
2025-01-14 09:42:59 +0530

    fix: fixed book.route & BaseUrl

--- frontend/src/utils/baseURL.js
+++ frontend/src/utils/baseURL.js
@@ -1,6 +1,10 @@
 const getBaseUrl = () => {
-    return 'http://localhost:5000'
-}
-
+    // Check if the environment is development or production
+    if (process.env.NODE_ENV === 'development') {
+        return 'http://localhost:5000';
+    } else {
+        return 'https://book-store-backend-gamma-self.vercel.app';
+    }
+};
 
 export default getBaseUrl;
